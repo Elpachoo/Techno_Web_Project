d5e6ec4067094bb9c0b229ade8cc746cbbb10957
Leo <leo.dedeine@isen.yncrea.fr>
2018-10-06 18:40:45 +0200

    Modif sur la page de recherche

---
+++ recherche.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Water Life</title>
+	<link rel="stylesheet" href="recherche.css">
+</head>
+<body>
+    <div id="bloc_page">
+    <?php include("header.php"); ?> 
+    <div id="titre_principal_inscription">
+    <h1>Bienvenue dans la page de recherche !
+        </h1>
+        <br>
+    <h2>Ici, faites vos recherches en toute tranquilité afin d'accéder à toute notre gamme de bouteille d'eau !
+        </h2>
+        <br>
+	<div id="searchbar">
+    	<span class="text">Saisissez votre recherche ici !</span>
+    	<form action="">
+        	<input class="champ" type="text" placeholder="Exemple: Evian.."/>
+        	<input class="bouton" type="image" src="pictures/search2.png" value=" "  /> 
+        	
+    	</form>
+        <br>
+        <br>
+        
+	</div>
+
+</body>
+</html>
