1b51f78aa3dd623d35a631c0e5171e5b15a69a73
Robin_GHYS <robinpourlesjeux@gmail.com>
2018-10-11 14:08:35 +0200

    debut recherche+php

--- recherche.php
+++ recherche.php
@@ -5,6 +5,18 @@
 	<title>Water Life</title>
 	<link rel="stylesheet" href="recherche.css">
 </head>
+
+<?php
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=ecommerce;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+?>
+
 <body>
     <div id="bloc_page">
     <?php include("header.php"); ?> 
@@ -17,14 +29,40 @@
         <br>
 	<div id="searchbar">
     	<span class="text">Saisissez votre recherche ici !</span>
-    	<form action="">
-        	<input class="champ" type="text" placeholder="Exemple: Evian.."/>
-        	<input class="bouton" type="image" src="pictures/search2.png" value=" "  /> 
+
+        <?php 
+        if(!isset($_POST['submit']))
+        {
+        ?>
+    	<form method="post" action="recherche.php">
+        	<input class="champ" type="text" name="recherche" placeholder="Exemple: Evian.."/>
+        	<input class="bouton" type="image" name="submit" src="pictures/search2.png" value=" "  /> 
         	
     	</form>
         <br>
         <br>
-        
+        <?php 
+        }
+        else {
+            $req = $bdd->prepare('SELECT * FROM user WHERE Prenom = ?');
+            $req->execute(array($_POST['recherche']));
+
+            while ($donnees = $req->fetch())
+{
+?>
+    <p>
+    <strong>Jeu</strong> : <?php echo $donnees['Nom']; ?><br />
+    Le possesseur de ce jeu est : <?php echo $donnees['Prenom']; ?>, son Login est  <?php echo $donnees['Login']; ?> euros !<br />
+    son password est <?php echo $donnees['Password']; ?> et on peut le contacter à <?php echo $donnees['Mail']; ?> au maximum<br />
+    <?php echo $donnees['PhoneNumber']; ?> c'est son numero telephone et il habite a  <?php echo $donnees['Adresse']; ?> Voila son code postal <?php echo $donnees['CodePostal']; ?> : <em> il habite en <?php echo $donnees['Pays']; ?></em>
+   </p>
+<?php
+}
+
+$req->closeCursor(); // Termine le traitement de la requête
+}
+?>
+        ?>        
 	</div>
 
 </body>
