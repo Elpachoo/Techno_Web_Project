22748a45b23b8c2eecd1249f94a2744879e9e427
Leo <leo.dedeine@isen.yncrea.fr>
2018-10-12 20:56:47 +0200

    Modif Panier

--- cart.php
+++ cart.php
@@ -27,7 +27,7 @@ catch(Exception $e)
 <body class="body">
 	<div class="legende"> 
     <?php 
-      $req=$bdd->query('SELECT products.Description AS description,panier.Prix AS prix, user.Prenom AS prenom_user, panier.Marque AS marque_bouteille, panier.Quantite AS quantite,products.Image AS image FROM panier, user,products WHERE panier.id_user = user.Id and panier.Marque=products.Marque');
+      $req=$bdd->query('SELECT products.Materiau AS materiau, products.Type AS type,products.Description AS description,panier.Prix AS prix, user.Prenom AS prenom_user, panier.Marque AS marque_bouteille, panier.Quantite AS quantite,products.Image AS image FROM panier, user,products WHERE panier.id_user = user.Id and panier.Marque=products.Marque');
       while($donnees=$req->fetch())
     {
     ?>
@@ -38,17 +38,15 @@ catch(Exception $e)
           <caption><strong>Panier de <?php echo $donnees['prenom_user']; ?></strong></caption>
 
               <tr>
-                    <th style="width:400px; height:50px;">Photo du Produit</th>
-                    <th style="width:300px">Nom du Produit</th>
-                    <th style="width:200px">Prix</th>
-                    <th style="width:200px">Quantité</th>
-                    <th>Actions</th>
-              </tr>
-              <tr>
-                    <td style="width:300px; "><img class="img" src="<?php echo $donnees['image']; ?>"></td>
-                    <td><?php echo $donnees['marque_bouteille']; ?> : <br> <?php echo $donnees['description']; ?></td>
-                    <td><?php echo $donnees['prix']; ?> € (T.T.C)</td>
-                    <td><?php echo $donnees['quantite'] ; ?></td>
+                    <td style="width:150px; height:150px; "><img class="img" src="<?php echo $donnees['image']; ?>"></td>
+                    <td class="description" style="height:100px"><?php echo $donnees['marque_bouteille']; ?> : <br> 
+                          <?php echo $donnees['description']; ?>. <br>
+                          Eau : <?php echo $donnees['type']; ?>. <br>
+                          Bouteille en : <?php echo $donnees['materiau']; ?></td>
+                    <td style="width:100px"></td>
+                    <td style="height:150px">Prix : <?php echo $donnees['prix']; ?> € (T.T.C)</td>
+                    <td style="width:100px"></td>
+                    <td>Quantité : <?php echo $donnees['quantite'] ; ?></td>
                     <td>  </td>
               </tr>
       </table>
