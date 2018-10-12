7e5d3f5da76eb8bcfa800cc20a181160d943d96f
Leo <leo.dedeine@isen.yncrea.fr>
2018-10-12 20:36:43 +0200

    Modifications du pannier

--- cart.php
+++ cart.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="css/cart.css">
   <link rel="stylesheet" type="text/css" href="css/header.css">
   <link rel="stylesheet" type="text/css" href="css/footer.css">
+  <link rel="stylesheet" type="text/css" href="css/fonts.css">
 </head>
 <?php
 try
@@ -23,20 +24,35 @@ catch(Exception $e)
 <header>
 	<?php include("header.php"); ?> 
 </header>
-<body>
+<body class="body">
 	<div class="legende"> 
     <?php 
-      $req=$bdd->query('SELECT user.Prenom AS prenom_user, panier.Marque AS marque_bouteille, panier.Quantite AS quantite FROM panier, user WHERE panier.id_user = user.Id ');
+      $req=$bdd->query('SELECT products.Description AS description,panier.Prix AS prix, user.Prenom AS prenom_user, panier.Marque AS marque_bouteille, panier.Quantite AS quantite,products.Image AS image FROM panier, user,products WHERE panier.id_user = user.Id and panier.Marque=products.Marque');
       while($donnees=$req->fetch())
     {
     ?>
     <br>
      <fieldset>                   
     <p>
-      <strong>Description</strong> :<br />
-      Ici pour le test, on affiche le panier de : <?php echo $donnees['prenom_user']; ?> .
-      Il a commande de la <?php echo $donnees['marque_bouteille']; ?>
-      Il en a commande <?php echo $donnees['quantite'] ; ?>
+      <table>
+          <caption><strong>Panier de <?php echo $donnees['prenom_user']; ?></strong></caption>
+
+              <tr>
+                    <th style="width:400px; height:50px;">Photo du Produit</th>
+                    <th style="width:300px">Nom du Produit</th>
+                    <th style="width:200px">Prix</th>
+                    <th style="width:200px">Quantité</th>
+                    <th>Actions</th>
+              </tr>
+              <tr>
+                    <td style="width:300px; "><img class="img" src="<?php echo $donnees['image']; ?>"></td>
+                    <td><?php echo $donnees['marque_bouteille']; ?> : <br> <?php echo $donnees['description']; ?></td>
+                    <td><?php echo $donnees['prix']; ?> € (T.T.C)</td>
+                    <td><?php echo $donnees['quantite'] ; ?></td>
+                    <td>  </td>
+              </tr>
+      </table>
+      
     </p>  
     </fieldset>
     <br>             
