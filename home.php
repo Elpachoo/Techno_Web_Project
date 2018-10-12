92d0d4bbe302c5671244f5051fa1411494149b5e
Leo <leo.dedeine@isen.yncrea.fr>
2018-10-12 16:35:39 +0200

    Creation Home

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Water Life</title>
+	<link rel="stylesheet" href="css/home.css">
+	<link rel="stylesheet" type="text/css" href="css/header.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+</head>
+<header>
+	<?php include("header.php"); ?> 
+</header>
+<body>
+	<br>
+	<br>
+	<a href="products.php"><img src="pictures/promoeaux.jpg" ></a>
+
+</body>
+<?php include('footer.php') ?>
+</html>
