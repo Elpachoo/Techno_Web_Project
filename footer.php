bc34fd18b486c967cdddf67b6b665e899c62f073
Unknown <haddadi.giovanni@gmail.com>
2018-10-26 13:48:28 +0200

    style des icônes, reste à changer les liens des autres pages

--- footer.php
+++ footer.php
@@ -11,10 +11,33 @@
         </div>
 
         <div class="logo_div">
-            <a href="https://twitter.com/" target="_blank" class="logo_a" id="logo_twitter"><img src="pictures\logo_twitter.png"  alt="Logo Twitter"  title="Twitter"></a>
-            <a href="https://facebook.com/" target="_blank" class="logo_a" id="logo_facebook"><img src="pictures\logo_facebook.png"   alt="Logo Facebook" title="Facebook"></a>
-            <a href="https://youtube.com/" target="_blank" class="logo_a" id="logo_youtube"><img src="pictures\logo_youtube.png"  alt="Logo Youtube"  title="Youtube"></a>
-            <a href="https://www.google.fr/maps" target="_blank" class="logo_a" id="logo_googlemaps"><img src="pictures\logo_googlemaps.png"  alt="Logo Google Maps"  title="Google Maps"></a>
+            <a href="https://twitter.com/" class="logo">
+                <div class="overlay" id="overlay-twitter">
+                    Twitter
+                </div>
+                <img src="pictures/logo_twitter.png" alt="Logo Twitter"  title="Twitter">
+            </a>
+
+            <a href="https://facebook.com/" class="logo">
+                <div class="overlay" id="overlay-facebook">
+                    Facebook
+                </div>
+                <img src="pictures/logo_facebook.png" alt="Logo Facebook" title="Facebook">
+            </a>
+
+            <a href="https://youtube.com/" class="logo">
+                <div class="overlay" id="overlay-youtube">
+                    Youtube
+                </div>
+                <img src="pictures/logo_youtube.png" alt="Logo Youtube"  title="Youtube">
+            </a>
+
+            <a href="https://www.google.fr/maps" class="logo">
+                <div class="overlay" id="overlay-google">
+                    Maps
+                </div>
+                <img src="pictures/logo_googlemaps.png" alt="Logo Google Maps"  title="Google Maps">
+            </a>
         </div>
         <p class="copyright_footer">© 2018-2019 Water Life ® - Tous droits réservés</p>
     </div>
