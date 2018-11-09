6c8b190b56ff5938cb9149b2c04dc27fadc91824
Robin_GHYS <robinpourlesjeux@gmail.com>
2018-11-09 14:11:49 +0100

    test2

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php // On se connecte à MySQL
-	      try{
-	        $bdd = new PDO('mysql:host=localhost;dbname=ecommerce;charset=utf8', 'root', '');
-	      }
-	      catch(Exception $e){
-	        // En cas d'erreur, on affiche un message et on arrête tout
-	        die('Erreur : '.$e->getMessage());
-	      }
-	    ?>
