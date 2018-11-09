ef2218b291e0647f355f5b48afa869d0d53d6fc9
Robin_GHYS <robinpourlesjeux@gmail.com>
2018-11-09 09:31:05 +0100

    groupement cart !

--- view/cart.php
+++ view/cart.php
@@ -16,7 +16,7 @@
       <h1 class="h1"> Mon Panier </h1>
     </div>
     <?php 
-      $req=$bdd->query('SELECT panier.Prix*panier.Quantite AS pri, products.Description AS description,panier.Prix AS prix, panier.Marque AS marque_bouteille, panier.Quantite AS quantite,products.Image AS image, panier.id AS idcommande FROM panier,products WHERE panier.id_user ='.$_SESSION['id_user'].' and panier.Marque=products.Marque');
+      $req=$bdd->query('SELECT panier.Prix*SUM(panier.Quantite) AS pri, products.Description AS description,panier.Prix AS prix, panier.Marque AS marque_bouteille, panier.Quantite AS quantite,products.Image AS image, panier.id AS idcommande, SUM(panier.Quantite) AS total FROM panier,products WHERE panier.id_user ='.$_SESSION['id_user'].' and panier.Marque=products.Marque GROUP BY(panier.Marque) ');
       while($donnees=$req->fetch())
     {
       $_SESSION['commandeid']=$donnees['idcommande'];
@@ -41,7 +41,7 @@
                     <td><div class="number-input">
                                 <button id="btnM" class="btnMoins" onclick="this.parentNode.querySelector('input[type=number]').stepDown()" ></button>
                             <form method="post" action="">
-                                  <input id="btnI" class="quantity" step="1" max="10" min="0"  name="modif_quantity" value="<?php echo $donnees['quantite']; ?>" type="number"> 
+                                  <input id="btnI" class="quantity" step="1" max="10" min="0"  name="modif_quantity" value="<?php echo $donnees['total']; ?>" type="number"> 
                             
                                   <input id="btnA" class="boutonAjouter" type="submit" name="modif_quantity_action"  value="Modifier" />
                                   
